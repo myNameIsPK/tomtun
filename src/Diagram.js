7e675c5e5abb11c8a62285cf8e505e2d3859c3ee
PK <zeropkza1@gmail.com>
2021-04-19 23:25:30 +0700

    update diagram template

--- src/Diagram.js
+++ src/Diagram.js
@@ -11,32 +11,32 @@ function init() {
     parameter1: 2,  // set the rounded corner
     spot1: go.Spot.TopLeft, spot2: go.Spot.BottomRight  // make content go all the way to inside edges of rounded corners
   };
-
+  
   const myDiagram =
-    $(go.Diagram,  // must name or refer to the DIV HTML element
-      {
-        "animationManager.initialAnimationStyle": go.AnimationManager.None,
-        "InitialAnimationStarting": function(e) {
-            var animation = e.subject.defaultAnimation;
-            animation.easing = go.Animation.EaseOutExpo;
-            animation.duration = 900;
-            animation.add(e.diagram, 'scale', 0.1, 1);
-            animation.add(e.diagram, 'opacity', 0, 1);
-        },
-
-        // have mouse wheel events zoom in and out instead of scroll up and down
-        "toolManager.mouseWheelBehavior": go.ToolManager.WheelZoom,
-        // support double-click in background creating a new node
-        "clickCreatingTool.archetypeNodeData": { text: "new node" },
-        // enable undo & redo
-        "undoManager.isEnabled": true,
-        positionComputation: function (diagram, pt) {
-          return new go.Point(Math.floor(pt.x), Math.floor(pt.y));
-        }
-      });
-
-  // when the document is modified, add a "*" to the title and enable the "Save" button
-  myDiagram.addDiagramListener("Modified", function (e) {
+  $(go.Diagram,  // must name or refer to the DIV HTML element
+    {
+      "animationManager.initialAnimationStyle": go.AnimationManager.None,
+      "InitialAnimationStarting": function(e) {
+        var animation = e.subject.defaultAnimation;
+        animation.easing = go.Animation.EaseOutExpo;
+        animation.duration = 900;
+        animation.add(e.diagram, 'scale', 0.1, 1);
+        animation.add(e.diagram, 'opacity', 0, 1);
+      },
+      
+      // have mouse wheel events zoom in and out instead of scroll up and down
+      "toolManager.mouseWheelBehavior": go.ToolManager.WheelZoom,
+      // support double-click in background creating a new node
+      "clickCreatingTool.archetypeNodeData": { text: "new node" },
+      // enable undo & redo
+      "undoManager.isEnabled": true,
+      positionComputation: function (diagram, pt) {
+        return new go.Point(Math.floor(pt.x), Math.floor(pt.y));
+      }
+    });
+    
+    // when the document is modified, add a "*" to the title and enable the "Save" button
+    myDiagram.addDiagramListener("Modified", function (e) {
     var button = document.getElementById("SaveButton");
     if (button) button.disabled = !myDiagram.isModified;
     var idx = document.title.indexOf("*");
@@ -47,6 +47,39 @@ function init() {
     }
   });
 
+  function hightLight(node) {
+    var shape = node.findObject("SHAPE");
+    shape.fill = "#6DAB80";
+    shape.stroke = "#A6E6A1";
+    var text = node.findObject("TEXT");
+    text.stroke = "white";
+
+    // node.findLinksOutOf().each( l => l.progress = "#52ce60")
+    for (const p of myDiagram.model.linkDataArray) {
+      if (p.from == node.key){
+        myDiagram.model.set(p, 'colorPath', "#52ce60");
+        myDiagram.model.set(p, 'colorText', "#52ce60");
+      }
+    }
+  };
+
+  function resetHightLight(node) {
+    var shape = node.findObject("SHAPE");
+    shape.fill = "#9e3309";
+    shape.stroke = null;
+    var text = node.findObject("TEXT");
+    text.stroke = "#ffffff";
+
+    // node.findLinksOutOf().each( l => l.progress = false)
+    for (const p of myDiagram.model.linkDataArray) {
+      if (p.from == node.key){
+        myDiagram.model.set(p, 'colorPath', null);
+        myDiagram.model.set(p, 'colorText', null);
+      }
+    }
+  };
+
+
   // define the Node template
   myDiagram.nodeTemplate =
     $(go.Node, "Auto",
@@ -54,13 +87,15 @@ function init() {
         locationSpot: go.Spot.Top,
         isShadowed: true, shadowBlur: 1,
         shadowOffset: new go.Point(0, 1),
-        shadowColor: "rgba(0, 0, 0, .14)"
+        shadowColor: "rgba(0, 0, 0, .14)",
+        mouseEnter: (e,node) => {hightLight(node)},
+        mouseLeave: (e,node) => {resetHightLight(node)},
       },
       new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),
       // define the node's outer shape, which will surround the TextBlock
-      $(go.Shape, "RoundedRectangle", roundedRectangleParams,
+      $(go.Shape, "Circle", roundedRectangleParams,
         {
-          name: "SHAPE", fill: "#ffffff", strokeWidth: 0,
+          name: "SHAPE", fill: "#9e3309", strokeWidth: 0,
           stroke: null,
           portId: "",  // this Shape is the Node's port, not the whole Node
           fromLinkable: true, fromLinkableSelfNode: true, fromLinkableDuplicates: true,
@@ -69,30 +104,13 @@ function init() {
         }),
       $(go.TextBlock,
         {
-          font: "bold small-caps 11pt helvetica, bold arial, sans-serif", margin: 7, stroke: "rgba(0, 0, 0, .87)",
+          name: "TEXT",
+          textAlign: "center",
+          font: "bold small-caps 11pt helvetica, bold arial, sans-serif", margin: 7, stroke: "#ffffff",
           editable: true  // editing the text automatically updates the model data
         },
         new go.Binding("text").makeTwoWay())
     );
-
-
-  // unlike the normal selection Adornment, this one includes a Button
-  myDiagram.nodeTemplate.selectionAdornmentTemplate =
-    $(go.Adornment, "Spot",
-      $(go.Panel, "Auto",
-        $(go.Shape, "RoundedRectangle", roundedRectangleParams,
-        { fill: null, stroke: "#7986cb", strokeWidth: 3 }),
-        $(go.Placeholder)  // a Placeholder sizes itself to the selected Node
-      ),
-      // the button to create a "next" node, at the top-right corner
-      $("Button",
-        {
-          alignment: go.Spot.TopRight,
-          click: addNodeAndLink  // this function is defined below
-        },
-        $(go.Shape, "PlusLine", { width: 6, height: 6 })
-      ) // end button
-    ); // end Adornment
 
   myDiagram.nodeTemplateMap.add("Start",
     $(go.Node, "Spot", { desiredSize: new go.Size(75, 75) },
@@ -135,50 +153,13 @@ function init() {
     )
   );
 
-
-  // clicking the button inserts a new node to the right of the selected node,
-  // and adds a link to that new node
-  function addNodeAndLink(e, obj) {
-    var adornment = obj.part;
-    var diagram = e.diagram;
-    diagram.startTransaction("Add State");
-
-    // get the node data for which the user clicked the button
-    var fromNode = adornment.adornedPart;
-    var fromData = fromNode.data;
-    // create a new "State" data object, positioned off to the right of the adorned Node
-    var toData = { text: "new" };
-    var p = fromNode.location.copy();
-    p.x += 200;
-    toData.loc = go.Point.stringify(p);  // the "loc" property is a string, not a Point object
-    // add the new node data to the model
-    var model = diagram.model;
-    model.addNodeData(toData);
-
-    // create a link data from the old node data to the new node data
-    var linkdata = {
-      from: model.getKeyForNodeData(fromData),  // or just: fromData.id
-      to: model.getKeyForNodeData(toData),
-      text: "transition"
-    };
-    // and add the link data to the model
-    model.addLinkData(linkdata);
-
-    // select the new Node
-    var newnode = diagram.findNodeForData(toData);
-    diagram.select(newnode);
-
-    diagram.commitTransaction("Add State");
-
-    // if the new node is off-screen, scroll the diagram to show the new node
-    diagram.scrollToRect(newnode.actualBounds);
-  }
-
   // replace the default Link template in the linkTemplateMap
   myDiagram.linkTemplate =
     $(go.Link,  // the whole link panel
       {
         curve: go.Link.Bezier,
+        // routing: go.Link.Orthogonal,
+        // curve: go.Link.JumpOver,
         adjusting: go.Link.Stretch,
         reshapable: true, relinkableFrom: true, relinkableTo: true,
         toShortLength: 3
@@ -187,10 +168,10 @@ function init() {
       new go.Binding("curviness"),
       $(go.Shape,  // the link shape
         { strokeWidth: 1.5 },
-        new go.Binding('stroke', 'progress', function(progress) {
+        new go.Binding('stroke', 'colorPath', function(progress) {
           return progress ? "#52ce60" /* green */ : 'black';
         }),
-        new go.Binding('strokeWidth', 'progress', function(progress) {
+        new go.Binding('strokeWidth', 'widthPath', function(progress) {
           return progress ? 2.5 : 1.5;
         })
         ),
@@ -200,21 +181,25 @@ function init() {
           return progress ? "#52ce60" /* green */ : 'black';
         })),
       $(go.Panel, "Auto",
-        $(go.Shape,  // the label background, which becomes transparent around the edges
-          {
-            fill: $(go.Brush, "Radial",
-              { 0: "rgb(245, 245, 245)", 0.7: "rgb(245, 245, 245)", 1: "rgba(245, 245, 245, 0)" }),
-            stroke: null
-          }),
+        // $(go.Shape,  // the label background, which becomes transparent around the edges
+        //   {
+        //     fill: $(go.Brush, "Radial",
+        //       { 0: "rgb(245, 245, 245)", 0.7: "rgb(245, 245, 245)", 1: "rgba(245, 245, 245, 0)" }),
+        //     stroke: null
+        //   }),
         $(go.TextBlock, "transition",  // the label text
           {
             textAlign: "center",
             font: "9pt helvetica, arial, sans-serif",
             margin: 4,
+            background: 'rgb(255, 207, 104)',
             editable: true  // enable in-place editing
           },
           // editing the text automatically updates the model data
-          new go.Binding("text").makeTwoWay())
+          new go.Binding("text").makeTwoWay(),
+          new go.Binding("stroke", 'colorText', function (progress) {
+            return progress ? '#52ce60' : "black";
+          }))
       )
     );
 
