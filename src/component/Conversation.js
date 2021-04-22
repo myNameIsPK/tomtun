f3f37e86616521b4cfffa7fe377f35c9ff6037cb
Chalermkiat Tiawpanichkij <extreme.gamie@gmail.com>
2021-04-22 19:11:05 +0700

    update css v3
    not have popup and aunt conver

--- src/component/Conversation.js
+++ src/component/Conversation.js
@@ -1,15 +1,14 @@
 import React,{useContext,useState} from 'react'
 import GlobalState from './GlobalState';
 
+import './cssFile/Conversation.css'
 
 const Conversation = () => {
     const [state, setState] = useContext(GlobalState);
  
     return (
         <div className = 'Aunt_conver'>
-            <span>
-                <img src= {state.pic} />
-            </span>
+            <img src= {state.pic} />
             <h1 className = 'aunt_conver'>
                 {state.conver}
             </h1>
