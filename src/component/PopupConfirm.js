99a3b3e26832649895e12f125024f6f6cf3755bb
Xeteo <50229026+Xeteo@users.noreply.github.com>
2021-04-22 19:52:45 +0700

    popup

--- src/component/PopupConfirm.js
+++ src/component/PopupConfirm.js
@@ -1,11 +1,12 @@
+import './cssFile/Popup.css'
 const PopupConfirm = ({toggle,pic,string}) => {
     return (
-        <div className='popup-container'>
-            <div className = 'box-popup'>
-                <img className = 'popup_pic' src={pic} />
-                <h1 className = 'popup_string'> {string} </h1>
-                <button className = 'take_order' onClick = {toggle}>รับอาหาร</button>
-            </div>
+        <div className = 'popup-container'>
+        <div className = 'box-popup'>
+             <img className = 'popup_pic' src={pic} />
+             <h1 className = 'popup_string'> {string} </h1>
+             <button className = 'take_order' onClick = {toggle} ><p>รับอาหาร</p></button>
+        </div>
         </div>
     )
 }
